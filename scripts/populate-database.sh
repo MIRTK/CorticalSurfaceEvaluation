c33826901409335a66d1b597a38468cfbca54e69
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-12-10 16:50:05 +0000

    enh: Take also screenshots of vol2mesh surfaces

--- scripts/populate-database.sh
+++ scripts/populate-database.sh
@@ -13,16 +13,20 @@ else
   SESSION='53600'
 fi
 
+SUBJECTS_CSV="$HOME/Datasets/dHCP/Info/subjects-v2.csv"
+
 BASE_DIR="$(dirname "$DATABASE")"
 SCRIPT_DIR="$(dirname "$BASH_SOURCE")"
-
-SUBJECTS_CSV="$HOME/Datasets/dHCP/Info/subjects-v2.csv"
+SCREENSHOTS_DIR="$BASE_DIR/{subject}-{session}/screenshots"
 
 IMAGE="$HOME/Experiments/dHCP/Surfaces/input/v2.3/dhcp-test/images/t2w/$SUBJECT-$SESSION.nii.gz"
 INITIAL_SURFACE="$HOME/Experiments/dHCP/Surfaces/derived/v2.3/dhcp-test/$SUBJECT-$SESSION/meshes/cerebrum.vtp"
 WHITE_MATTER_SURFACE="$HOME/Experiments/dHCP/Surfaces/derived/v2.3/dhcp-test/$SUBJECT-$SESSION/meshes/white.vtp"
-SCREENSHOTS_DIR="$BASE_DIR/{subject}-{session}/screenshots"
+VOL2MESH_SURFACE="$HOME/Experiments/dHCP/Surfaces/vol2mesh/v2.3/dhcp-test/$SUBJECT-$SESSION/white.vtp"
 
+MIN_DISTANCE=(2 1)
+ROI_SPAN=40
+MAX_SCAN_ROIS=20
 MIN_INTENSITY=10
 MAX_INTENSITY=30
 LINE_WIDTH=4
@@ -35,40 +39,59 @@ mkdir -p "$BASE_DIR" || exit 1
 if [ ! -f "$DATABASE" ]; then
   "$SCRIPT_DIR/create-tables.py" "$DATABASE" || exit 1
   "$SCRIPT_DIR/import-scans.py" "$SUBJECTS_CSV" "$DATABASE" || exit 1
-  "$SCRIPT_DIR/add-rater.py" "$DATABASE" \
-      --email 'andreas.schuh@imperial.ac.uk' \
-      --password 'test' \
-      --first-name 'Andreas' \
-      --last-name 'Schuh' \
-      --affiliation 'Imperial College London' \
-      || exit 1
 fi
 
 INITIAL_SURFACE_ID=$(sqlite3 "$DATABASE" "SELECT OverlayId FROM Overlays WHERE Name = 'Initial surface'")
 WHITE_MATTER_SURFACE_ID=$(sqlite3 "$DATABASE" "SELECT OverlayId FROM Overlays WHERE Name = 'White matter surface'")
+VOL2MESH_SURFACE_ID=$(sqlite3 "$DATABASE" "SELECT OverlayId FROM Overlays WHERE Name = 'Vol2mesh surface'")
 
 echo
 echo "Initial surface ID      = $INITIAL_SURFACE_ID"
 echo "White matter surface ID = $WHITE_MATTER_SURFACE_ID"
+echo "Vol2mesh surface ID     = $VOL2MESH_SURFACE_ID"
 echo
 
 # select regions of interest
 get_number_of_rois()
 {
-  sqlite3 "$DATABASE" "
-      SELECT COUNT(ROI_Id) AS NumROIs
-      FROM ROIs INNER JOIN Scans
-      ON ROIs.ScanId = Scans.ScanId
+  sqlite3 "$DATABASE" "\
+      SELECT COUNT(ROI_Id) AS NumROIs \
+      FROM ROIs INNER JOIN Scans \
+      ON ROIs.ScanId = Scans.ScanId \
       AND SubjectId = '$SUBJECT' AND SessionId = $SESSION"
 }
 n=$(get_number_of_rois)
 if [ $n -eq 0 ]; then
-  "$SCRIPT_DIR/select-regions-of-interest.py" "$DATABASE" $VERBOSE_FLAGS \
-      --subject "$SUBJECT" --session "$SESSION" \
-      --surface "$WHITE_MATTER_SURFACE" --reference "$INITIAL_SURFACE" \
-      --min-distance 1 --min-patch-size 10 --min-patch-area 1 --max-scan-rois 20 --roi-size 30 \
-      || exit 1
-  n=$(get_number_of_rois)
+  CURRENT_DATABASE="$DATABASE"
+  DATABASE="${CURRENT_DATABASE/.db/.next.db}"
+  cp "$CURRENT_DATABASE" "$DATABASE"
+  for d in ${MIN_DISTANCE[@]}; do
+    echo "Select regions with minimum distance = $d..."
+    "$SCRIPT_DIR/select-regions-of-interest.py" "$DATABASE" $VERBOSE_FLAGS \
+        --subject "$SUBJECT" --session "$SESSION" \
+        --surface "$WHITE_MATTER_SURFACE" --reference "$VOL2MESH_SURFACE" \
+        --min-distance ${MIN_DISTANCE[0]} --min-patch-size 10 --min-patch-area 1 \
+        --max-scan-rois $MAX_SCAN_ROIS --roi-size $ROI_SPAN \
+        || exit 1
+    m=$(get_number_of_rois)
+    if [ $n -eq $m ]; then
+      break
+    fi
+    n=$m
+    "$SCRIPT_DIR/select-regions-of-interest.py" "$DATABASE" $VERBOSE_FLAGS \
+        --subject "$SUBJECT" --session "$SESSION" \
+        --surface "$WHITE_MATTER_SURFACE" --reference "$INITIAL_SURFACE" \
+        --min-distance ${MIN_DISTANCE[0]} --min-patch-size 10 --min-patch-area 1 \
+        --max-scan-rois $MAX_SCAN_ROIS --roi-size $ROI_SPAN \
+        || exit 1
+    m=$(get_number_of_rois)
+    if [ $n -eq $m ]; then
+      break
+    fi
+    n=$m
+  done
+  mv "$DATABASE" "$CURRENT_DATABASE"
+  DATABASE="$CURRENT_DATABASE"
   echo -n "Added"
 else
   echo -n "Found"
@@ -78,11 +101,11 @@ echo " $n regions of interest in database"
 # save screenshots of whole slices with bounding boxes overlaid
 get_number_of_screenshots_with_bounding_boxes()
 {
-  sqlite3 "$DATABASE" "
-    SELECT COUNT(ScreenshotId) AS NumScreenshots
-    FROM ROIScreenshots AS S
-    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id
-    INNER JOIN Scans AS I ON I.ScanId = R.ScanId
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM ROIScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
     WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION"
 }
 n=$(get_number_of_screenshots_with_bounding_boxes)
@@ -101,12 +124,12 @@ echo " $n screenshots with ROI bounding boxes in database"
 # save screenshots with initial surface overlaid
 get_number_of_screenshots_with_initial_surface()
 {
-  sqlite3 "$DATABASE" "
-    SELECT COUNT(ScreenshotId) AS NumScreenshots
-    FROM EvaluationScreenshots AS S
-    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id
-    INNER JOIN Scans AS I ON I.ScanId = R.ScanId
-    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM EvaluationScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
+    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION \
     AND OverlayId = $INITIAL_SURFACE_ID"
 }
 n=$(get_number_of_screenshots_with_initial_surface)
@@ -127,12 +150,12 @@ echo " $n screenshots with initial surface overlaid in database"
 # save screenshots with white matter surface overlaid
 get_number_of_screenshots_with_white_matter_surface()
 {
-  sqlite3 "$DATABASE" "
-    SELECT COUNT(ScreenshotId) AS NumScreenshots
-    FROM EvaluationScreenshots AS S
-    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id
-    INNER JOIN Scans AS I ON I.ScanId = R.ScanId
-    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM EvaluationScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
+    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION \
     AND OverlayId = $WHITE_MATTER_SURFACE_ID"
 }
 n=$(get_number_of_screenshots_with_white_matter_surface)
@@ -150,16 +173,42 @@ else
 fi
 echo " $n screenshots with white matter surface overlaid in database"
 
+# save screenshots with vol2mesh surface overlaid
+get_number_of_screenshots_with_vol2mesh_surface()
+{
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM EvaluationScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
+    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION \
+    AND OverlayId = $VOL2MESH_SURFACE_ID"
+}
+n=$(get_number_of_screenshots_with_vol2mesh_surface)
+if [ $n -eq 0 ]; then
+  "$SCRIPT_DIR/take-screenshots.py" "$DATABASE" $VERBOSE_FLAGS \
+      --subject "$SUBJECT" --session "$SESSION" --image "$IMAGE" \
+      --overlay $VOL2MESH_SURFACE_ID "$VOL2MESH_SURFACE" \
+      --prefix "$SCREENSHOTS_DIR/roi-vol2mesh-surface" \
+      --range $MIN_INTENSITY $MAX_INTENSITY \
+      || exit 1
+  n=$(get_number_of_screenshots_with_vol2mesh_surface)
+  echo -n "Added"
+else
+  echo -n "Found"
+fi
+echo " $n screenshots with vol2mesh surface overlaid in database"
+
 # save screenshots with both initial and white matter surface overlaid
 get_number_of_screenshots_with_initial_and_white_matter_surface()
 {
-  sqlite3 "$DATABASE" "
-    SELECT COUNT(ScreenshotId) AS NumScreenshots
-    FROM ComparisonScreenshots AS S
-    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id
-    INNER JOIN Scans AS I ON I.ScanId = R.ScanId
-    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION
-    AND OverlayId1 = $INITIAL_SURFACE_ID
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM ComparisonScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
+    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION \
+    AND OverlayId1 = $INITIAL_SURFACE_ID \
     AND OverlayId2 = $WHITE_MATTER_SURFACE_ID"
 }
 n=$(get_number_of_screenshots_with_initial_and_white_matter_surface)
@@ -177,3 +226,31 @@ else
   echo -n "Found"
 fi
 echo " $n screenshots with both initial and white matter surfaces overlaid in database"
+
+# save screenshots with both vol2mesh and white matter surface overlaid
+get_number_of_screenshots_with_vol2mesh_and_white_matter_surface()
+{
+  sqlite3 "$DATABASE" "\
+    SELECT COUNT(ScreenshotId) AS NumScreenshots \
+    FROM ComparisonScreenshots AS S \
+    INNER JOIN ROIs  AS R ON R.ROI_Id = S.ROI_Id \
+    INNER JOIN Scans AS I ON I.ScanId = R.ScanId \
+    WHERE SubjectId = '$SUBJECT' AND SessionId = $SESSION \
+    AND OverlayId1 = $WHITE_MATTER_SURFACE_ID \
+    AND OverlayId2 = $VOL2MESH_SURFACE_ID"
+}
+n=$(get_number_of_screenshots_with_vol2mesh_and_white_matter_surface)
+if [ $n -eq 0 ]; then
+  "$SCRIPT_DIR/take-screenshots.py" "$DATABASE" $VERBOSE_FLAGS \
+      --subject "$SUBJECT" --session "$SESSION" --image "$IMAGE" \
+      --overlay $WHITE_MATTER_SURFACE_ID "$WHITE_MATTER_SURFACE" \
+      --overlay $VOL2MESH_SURFACE_ID "$VOL2MESH_SURFACE" \
+      --prefix "$SCREENSHOTS_DIR/roi-vol2mesh-and-white-matter-surface" \
+      --range $MIN_INTENSITY $MAX_INTENSITY --shuffle-colors --line-width $LINE_WIDTH \
+      || exit 1
+  n=$(get_number_of_screenshots_with_vol2mesh_and_white_matter_surface)
+  echo -n "Added"
+else
+  echo -n "Found"
+fi
+echo " $n screenshots with both vol2mesh and white matter surfaces overlaid in database"
