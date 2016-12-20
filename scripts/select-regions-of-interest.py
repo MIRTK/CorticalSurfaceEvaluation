ecd622455c9bc9fb0aa52c89f8916d2504d8dbfd
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-12-20 00:41:00 +0000

    enh: Random selection of ROI center points

--- scripts/select-regions-of-interest.py
+++ scripts/select-regions-of-interest.py
@@ -6,7 +6,7 @@ import os
 import sqlite3
 import argparse
 
-from vtk import (vtkXMLPolyDataReader, vtkXMLPolyDataWriter,
+from vtk import (vtkXMLPolyDataReader, vtkXMLPolyDataWriter, vtkPolyData, vtkMaskPoints,
                  vtkDistancePolyDataFilter, vtkPolyDataConnectivityFilter, vtkPointDataToCellData,
                  vtkIdList, vtkExtractCells, vtkDataSetSurfaceFilter, vtkMassProperties)
 
@@ -60,7 +60,7 @@ def get_max_distance(distances, region_ids, region_id):
     """Get max point distance of specified cluster."""
     d = 0
     i = -1
-    for point_id in xrange(region_ids.GetNumberOfTuples()):
+    for point_id in range(region_ids.GetNumberOfTuples()):
         if region_ids.GetComponent(point_id, 0) == float(region_id):
             distance = distances.GetComponent(point_id, 0)
             if distance > d:
@@ -74,14 +74,14 @@ def compute_area(surface, region_ids, region_id, cell_data=False):
     cell_ids = vtkIdList()
     cell_ids.Allocate(surface.GetNumberOfCells())
     if cell_data:
-        for cell_id in xrange(surface.GetNumberOfCells()):
+        for cell_id in range(surface.GetNumberOfCells()):
             if int(region_ids.GetComponent(cell_id, 0)) == region_id:
                 cell_ids.InsertNextId(cell_id)
     else:
         surface.BuildLinks()
         point_ids = vtkIdList()
         point_ids.Allocate(10)
-        for cell_id in xrange(surface.GetNumberOfCells()):
+        for cell_id in range(surface.GetNumberOfCells()):
             surface.GetCellPoints(cell_id, point_ids)
             exclude = False
             for i in range(point_ids.GetNumberOfIds()):
@@ -106,24 +106,43 @@ def compute_area(surface, region_ids, region_id, cell_data=False):
     return props.GetSurfaceArea()
 
 
-def select_points(surface_name, reference_name, output_name=None,
+def read_surface_mesh(surface_name):
+    """Read surface mesh from file without point/cell data."""
+    surface_reader = vtkXMLPolyDataReader()
+    surface_reader.SetFileName(surface_name)
+    surface_reader.UpdateWholeExtent()
+    surface = vtkPolyData()
+    surface.DeepCopy(surface_reader.GetOutput())
+    surface.GetPointData().Initialize()
+    surface.GetCellData().Initialize()
+    return surface
+
+
+def sample_points(surface, n=100, stratified=True):
+    """Randomly sample points."""
+    sampler = vtkMaskPoints()
+    sampler.SetInputData(surface)
+    sampler.SetMaximumNumberOfPoints(n)
+    sampler.RandomModeOn()
+    if stratified:
+        sampler.SetRandomModeType(2)
+    else:
+        sampler.SetRandomModeType(1)
+    sampler.Update()
+    output = sampler.GetOutput()
+    points = []
+    p = [0, 0, 0]
+    for i in range(output.GetNumberOfPoints()):
+        output.GetPoint(i, p)
+        points.append(tuple(p))
+    return points
+
+
+def select_points(surface, reference, output_name=None,
                   min_distance=1., max_distance=float('inf'),
                   min_patch_size=1, min_patch_area=0.):
     """Select surface points based on minimum distance from reference surface."""
     min_patch_size = max(1, min_patch_size)
-    # read input surfaces
-    surface_reader = vtkXMLPolyDataReader()
-    surface_reader.SetFileName(surface_name)
-    surface_reader.UpdateWholeExtent()
-    surface = surface_reader.GetOutput()
-    surface.GetPointData().Initialize()
-    surface.GetCellData().Initialize()
-    reference_reader = vtkXMLPolyDataReader()
-    reference_reader.SetFileName(reference_name)
-    reference_reader.UpdateWholeExtent()
-    reference = reference_reader.GetOutput()
-    reference.GetPointData().Initialize()
-    reference.GetCellData().Initialize()
     # compute inter-surface distance
     calc = vtkDistancePolyDataFilter()
     calc.SetInputData(0, surface)
@@ -165,7 +184,7 @@ def select_points(surface_name, reference_name, output_name=None,
         region_ids = point_region_ids
         cell_region_ids = False
     regions = []
-    for region_id in xrange(conn.GetNumberOfExtractedRegions()):
+    for region_id in range(conn.GetNumberOfExtractedRegions()):
         conn.DeleteSpecifiedRegion(region_id)
         patch_size = conn.GetRegionSizes().GetValue(region_id)
         if patch_size > min_patch_size:
@@ -178,8 +197,7 @@ def select_points(surface_name, reference_name, output_name=None,
     regions.sort(key=lambda x: x[3], reverse=True)
     points = []
     p = [0, 0, 0]
-    surface = surface_reader.GetOutput()
-    for i in xrange(len(regions)):
+    for i in range(len(regions)):
         surface.GetPoint(regions[i][1], p)
         points.append(tuple(p))
     return points
@@ -242,21 +260,30 @@ def filter_points(db, scan_id, points, span, max_overlap, max_number_of_points):
     result = []
     if max_number_of_points == 0:
         return result
-    for point in points:
+    for i in range(len(points)):
+        point = points[i]
         box1 = bounding_box(center=point, span=span)
         vol1 = compute_volume(box1)
         keep = True
-        for roi in query_overlapping_rois(db, scan_id, box1, cols='CenterX, CenterY, CenterZ, Span'):
-            box2 = bounding_box(center=(roi[0], roi[1], roi[2]), span=roi[3])
+        for j in range(i + 1, len(points)):
+            box2 = bounding_box(center=points[j], span=span)
             vol2 = compute_volume(box2)
             overlap = compute_overlap(box1, vol1, box2, vol2)
             if overlap > max_overlap:
                 keep = False
                 break
         if keep:
-            result.append(point)
-            if max_number_of_points > 0 and len(result) >= max_number_of_points:
-                break
+            for roi in query_overlapping_rois(db, scan_id, box1, cols='CenterX, CenterY, CenterZ, Span'):
+                box2 = bounding_box(center=(roi[0], roi[1], roi[2]), span=roi[3])
+                vol2 = compute_volume(box2)
+                overlap = compute_overlap(box1, vol1, box2, vol2)
+                if overlap > max_overlap:
+                    keep = False
+                    break
+            if keep:
+                result.append(point)
+                if max_number_of_points > 0 and len(result) >= max_number_of_points:
+                    break
     return result
 
 
@@ -280,8 +307,9 @@ if __name__ == '__main__':
     parser.add_argument('--subject', help="Subject ID", required=True)
     parser.add_argument('--session', help="Session ID", required=True)
     parser.add_argument('--surface', help="Surface mesh file", required=True)
-    parser.add_argument('--reference', help="Reference surface mesh file", required=True)
+    parser.add_argument('--reference', help="Reference surface mesh file")
     parser.add_argument('--output', help="Output mesh file with computed surface distances and clusters")
+    parser.add_argument('--random', default=0, type=int, help="Randomly sample the specified number of points")
     parser.add_argument('--min-distance', default=1., type=float, help="Minimum distance to reference in mm")
     parser.add_argument('--max-distance', default=float('inf'), type=float, help="Maximum distance to reference in mm")
     parser.add_argument('--min-patch-size', default=10, type=int, help="Minimum number of distant surface patch points")
@@ -308,38 +336,50 @@ if __name__ == '__main__':
     scan_id = get_scan_id(cur, args.subject, args.session)
     cmd_id = get_or_insert_command_id(
         cur, name=os.path.basename(__file__), params=options(args, exclude=[
-            'database', 'surface', 'reference', 'output',
-            'subject', 'session', 'max_new_rois', 'max_scan_rois',
-            'print_sql', 'verbose'
+            'database', 'output', 'subject', 'session', 'print_sql', 'verbose'
         ]),
         print_sql=args.print_sql
     )
-    # select surface points
-    points = select_points(args.surface, args.reference,
-                           output_name=args.output,
-                           min_distance=args.min_distance,
-                           max_distance=args.max_distance,
-                           min_patch_size=args.min_patch_size,
-                           min_patch_area=args.min_patch_area)
-    if args.verbose > 0:
-        print("Identified {} regions of interest".format(len(points)))
-    # remove points close to/overlapping with existing region of interest
+    # read surfaces
+    surface = read_surface_mesh(args.surface)
+    points = []
+    # select distant surface points
+    if args.reference:
+        reference = read_surface_mesh(args.reference)
+        samples = select_points(surface, reference,
+                                output_name=args.output,
+                                min_distance=args.min_distance,
+                                max_distance=args.max_distance,
+                                min_patch_size=args.min_patch_size,
+                                min_patch_area=args.min_patch_area)
+        if args.verbose > 0:
+            print("Identified {} distant points".format(len(samples)))
+        points.extend(samples)
+    # select further random surface points
+    if args.random > 0:
+        samples = sample_points(surface, n=args.random)
+        if args.verbose > 0:
+            print("Randomly selected {} surface points".format(len(samples)))
+        points.extend(samples)
+    # remove points close to/overlapping with other/existing regions of interest
     num_scan_rois = get_number_of_rois(cur, scan_id)
     if args.max_new_rois > 0:
         max_new_rois = args.max_new_rois
     else:
         max_new_rois = -1
     if args.max_scan_rois > 0 and args.max_scan_rois - num_scan_rois:
-        max_new_rois = max(0, args.max_scan_rois - num_scan_rois)
+        diff = max(0, args.max_scan_rois - num_scan_rois)
+        if max_new_rois > 0:
+            max_new_rois = min(max_new_rois, diff)
+        else:
+            max_new_rois = diff
     points = filter_points(cur, scan_id=scan_id, points=points, span=args.span,
                            max_overlap=args.max_overlap_ratio, max_number_of_points=max_new_rois)
-    if args.verbose > 1:
+    if args.verbose > 0:
         print("Selected {} regions of interest".format(len(points)))
     # write selected regions of interest to database
     for p in points:
         insert_roi(cur, scan_id=scan_id, center=p, span=args.span, cmd_id=cmd_id, print_sql=args.print_sql)
-    if args.verbose > 0:
-        print("Inserted {} new regions of interest".format(len(points)))
 
     cur.close()
     con.commit()
