320e4eb5db1b14109efeff89e163b5a6772dba9d
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-01-06 17:51:41 +0000

    fix: App build.js script

--- app/build.js
+++ app/build.js
@@ -1,23 +1,42 @@
 var path = require('path');
 var NwBuilder = require('nw-builder');
 
-var args = process.argv.slice(2);
-if (args.length === 0) {
+var platforms = process.argv.slice(2);
+if (platforms.length === 0) {
   if (process.platform === 'darwin') {
-    args.push('osx64');
+    platforms.push('osx64');
   } else if (process.platform === 'linux') {
-    args.push('linux64');
+    platforms.push('linux64');
   } else if (process.platform === 'win32') {
-    args.push('win64');
+    platforms.push('win64');
   }
 }
 
+var files = [
+  'package.json',
+  'index.html',
+  'app.css',
+  'app.js',
+  'compare-contours.png',
+  'evaluate-accuracy.png',
+  'node_modules/jquery/**',
+  'node_modules/bootstrap/**',
+  'node_modules/tether/**',
+  'node_modules/nan/**',
+  'node_modules/sqlite3/**'
+];
+
+for (var i = 0; i < files.length; i++) {
+  files[i] = path.join(__dirname, files[i]);
+}
+
 var nw = new NwBuilder({
-  files: path.join(__dirname, '**'),
-  platforms: args,
+  files: files,
+  platforms: platforms,
   version: '0.19.1',
   flavor: 'normal',
   buildDir: __dirname,
+  cacheDir: path.join(__dirname, 'build', 'cache'),
   buildType: function () { return 'build'; },
   macIcns: path.join(__dirname, 'app.icns'),
   winIco: path.join(__dirname, 'app.ico'),
